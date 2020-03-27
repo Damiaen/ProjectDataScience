4b86058512b36b334058c6982d575f884c42f546
Damiaen <2Csu6ygqGZwvMsB>
2020-03-27 13:48:43 +0100

    Added UI for Parser and Database builder

--- src/main/java/com/datascience/bigmovie/base/Main.java
+++ src/main/java/com/datascience/bigmovie/base/Main.java
@@ -2,6 +2,7 @@ package com.datascience.bigmovie.base;
 
 import com.datascience.bigmovie.base.UserInterface.UserInterface;
 
+import javax.swing.*;
 import java.io.*;
 
 /**
@@ -14,8 +15,11 @@ public class Main {
      */
     public static void main(String[] args) {
         try {
-            new UserInterface().main();
-        } catch (IOException e) {
+            // Force Windows style for the Swing application
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            // Start the main UI
+            new UserInterface();
+        } catch (IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/com/datascience/bigmovie/base/UserInterface/UserInterface.java
+++ src/main/java/com/datascience/bigmovie/base/UserInterface/UserInterface.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Objects;
 
-public class UserInterface {
+public class UserInterface extends JFrame{
     private JPanel rootPanel;
     private JButton button_parse;
     private JButton button_build;
@@ -18,7 +18,6 @@ public class UserInterface {
     private JButton ask_question;
     private JPanel content_panel;
     private JTextPane answer_content;
-    private JProgressBar progressBar;
 
     public String[] questionSelect = {
         "1. Welke actrices en acteurs spelen in meer dan 15 films met een ranking vanaf 8 sterren?",
@@ -30,18 +29,23 @@ public class UserInterface {
         "7. Geef top 15 lijst met films met een ranking vanaf 9,5sterren maar hebben het laagste productiebudget?",
     };
 
-    public UserInterface() {
+    /**
+     * Main JFrame IU, everything in the view is stored here
+     */
+    private JFrame userInterfaceFrame = new JFrame("Project DataScience - Groep 4");
+
+    public UserInterface(){
+        // Create required elements for the JFrame
+        createInterfaceElements();
+
+        // Event listeners that respond to button clicks
         button_parse.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                runParser();
-            }
+            public void actionPerformed(ActionEvent actionEvent) { runParser(); }
         });
         button_build.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                runDatabaseBuilder();
-            }
+            public void actionPerformed(ActionEvent actionEvent) { runDatabaseBuilder(); }
         });
         ask_question.addActionListener(new ActionListener() {
             @Override
@@ -50,60 +54,42 @@ public class UserInterface {
             }
         });
 
+        // Set Combobox content
         DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>(questionSelect);
         questions_list.setModel(model);
     }
 
-    public void main() throws IOException{
-        try {
-            // Force windows style upon the graphical interface
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-            createUI();
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
+    /**
+     * Main function that contains all of the base UI settings
+     */
+    private void createInterfaceElements() {
+        userInterfaceFrame.setContentPane(rootPanel);
+        userInterfaceFrame.setSize(720,480);
+        userInterfaceFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        userInterfaceFrame.setVisible(true);
     }
 
-    private void createUI() {
-        JFrame frame = new JFrame("Project DataScience - Groep 4");
-        frame.setContentPane(new UserInterface().rootPanel);
-        frame.setSize(720,480);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-    }
-
+    /**
+     * Dispose of the main UI and open the Parser UI
+     */
     private void runParser() {
-        // Hide elements
-        questions_panel.setVisible(false);
-        answer_content.setVisible(false);
-        buttons_panel.setVisible(false);
-        // Show loader
-        progressBar.setVisible(true);
-        progressBar.setIndeterminate(true);
-
-        new SwingWorker<Void, String>() {
-            @Override
-            protected Void doInBackground() throws Exception {
-                new Parser().setupParser();
-                return null;
-            }
-
-            @Override
-            protected void done() {
-                // Turn of loader
-                progressBar.setIndeterminate(false);
-                progressBar.setVisible(false);
-                // Show elements
-                questions_panel.setVisible(true);
-                answer_content.setVisible(true);
-            }
-        }.execute();
+        userInterfaceFrame.setVisible(false);
+        new ParserInterface();
+        userInterfaceFrame.dispose();
     }
 
+    /**
+     * Dispose of the main UI and open the DatabaseBuilder UI
+     */
     private void runDatabaseBuilder() {
-        System.out.println("Starting up Database builder");
+        userInterfaceFrame.setVisible(false);
+        new DatabaseInterface();
+        userInterfaceFrame.dispose();
     }
 
+    /**
+     * Run the question Builder function and ask an question to the database
+     */
     private void runQuestionBuilder() {
         String comboBoxValue = Objects.requireNonNull(questions_list.getSelectedItem()).toString();
         int comboBoxSelectedIndex = questions_list.getSelectedIndex();
